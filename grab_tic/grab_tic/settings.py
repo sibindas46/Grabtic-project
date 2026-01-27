2967ed38a6be59fbec29e4e97a72333cd10cdd19
Sibindas <sibindas46@gmail.com>
2026-01-27 10:48:18 +0530

    initial hosting

--- grab_tic/grab_tic/settings.py
+++ grab_tic/grab_tic/settings.py
@@ -83,22 +83,26 @@ WSGI_APPLICATION = 'grab_tic.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':'3306',
-    }
-}
+if config('ENVIRONMENT') == 'production':
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+                    }
+             }
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST':config('DB_HOST'),
+            'PORT':'3306',
+                    }
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
